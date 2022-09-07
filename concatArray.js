16a45ab6e75518ca72352b06d5d3d395bc75c912
wachipor <wachipor@gmail.com>
2022-09-07 14:56:59 +0700

    test

--- concatArray.js
+++ concatArray.js
@@ -1,6 +1,23 @@
-const { template } = require('@babel/core')
+// const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
-  //code here
+  return array1+array2
 }
-module.exports = concatArray
+
+let array1 = []
+let array2 = []
+
+if(array1==[null]||[undefined] && [array2==null]||[undefined]) {
+  return 'undefined'
+}
+else if(array1 && array2 ==[]){
+ return 'empty array'
+}
+else{
+  return concatArray()
+}
+
+console.log(concatArray())
+  
+
+// module.exports = concatArray
